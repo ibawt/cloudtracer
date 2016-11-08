375e3f924ee80198274a0f43e906c49fca1b4fea
Ian Quick <ian.quick@gmail.com>
2016-11-08 18:27:47 -0500

    this was wrong

--- lib/cloudtracer/middleware.rb
+++ lib/cloudtracer/middleware.rb
@@ -6,7 +6,7 @@ module Cloudtracer
 
     def call(env)
       header = env['HTTP_X_CLOUD_TRACE_CONTEXT']
-      return @app.call unless header
+      return @app.call(env) unless header
 
       TraceContext.new(header).with_trace do
         @app.call(env)
